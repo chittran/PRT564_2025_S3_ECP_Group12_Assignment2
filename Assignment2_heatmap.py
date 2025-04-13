596a80868a8d874592b8cb58a22016670022e026
Chitt <tranthuychi3597@gmail.com>
2025-04-13 17:34:59 +0930

    update

--- Assignment2_heatmap.py
+++ Assignment2_heatmap.py
@@ -1,16 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import pathlib
-
-# Read dataset into a DataFrame
-df = pd.read_csv("Avian_Disease_Dataset.csv", encoding='ISO-8859-1')
-
-# Separate explanatory variables (x) from the response variable (y)
-x = df.iloc[:,3:-1].values
-y = df.iloc[:,-1].values
-
-Clinical_Sign = df.columns[3:-1].values
-print(Clinical_Sign)
+from Assignment2_Explore_relationship import df, Clinical_Sign
 
 # Plot the matrix as a heatmap to check remove multicollinearity
 pathlib.Path('output/correlation/').mkdir(parents=True, exist_ok=True) 
